14dffdd2be57ff4fa3e31fa99ed97fd09546c90f
lanza10 <jlanza100@gmail.com>
2023-11-29 14:24:50 +0100

    xml

--- back-end/extractors/xml_extractor.py
+++ back-end/extractors/xml_extractor.py
@@ -1,9 +1,12 @@
 import xml.etree.ElementTree as ET
 from models.data_model import DataModel
+from extractors.validations import Validations
 
 class XMLExtractor:
     def extract_data(self, file_path):
         data = []
+        errors = []
+        validations = Validations()
         tree = ET.parse(file_path)
         root = tree.getroot()
 
@@ -17,22 +20,53 @@ class XMLExtractor:
                 row_data[element.tag] = element.text
 
             # Crear un objeto DataModel a partir de los datos de la fila
-            localidad = {'codigo': '', 'nombre': row_data.get('nom_municipi')}
-            provincia = {
-                'codigo': row_data.get('codi_postal'), 'nombre': row_data.get('codi_postal')}
-            tipo = row_data.get('nom_naturalesa')
-            if tipo == 'Privat':
-                tipo = 'Privado'
-            elif tipo == 'Públic':
-                tipo = 'Público'
+            if 'codi_postal' in row_data & 'nom_municipi' in row_data :
+                localidad = {'codigo': '', 'nombre': row_data.get('nom_municipi')}
+                provincia = {
+                    'codigo': row_data.get('codi_postal'), 'nombre': row_data.get('codi_postal')}
+            
+            if 'nom_naturalesa' in row_data:
+                tipo = row_data.get('nom_naturalesa')
+                if tipo == 'Privat':
+                    tipo = 'Privado'
+                elif tipo == 'Públic':
+                    tipo = 'Público'
+
+            # Detect name errors
+            nombre = None
+            if 'denominaci_completa' in row_data:
+                nombre = row_data.get('denominaci_completa')
+                if not validations.isValidString(nombre):
+                    errors.append('El nombre del centro "' + nombre + '" es inválido.')
+            # Detect LONG AND LAT errors
+            if 'coordenades_geo_x' in row_data & 'coordenades_geo_y' in row_data :
+                    lon = row_data.get('coordenades_geo_x'),
+                    lat = row_data.get('coordenades_geo_y'),
+            else:
+                    continue
+            
+            # Detect address errors
+            direccion = None
+            if 'adre_a' in row_data:
+                direccion = row_data.get('adre_a')
+                if not validations.isValidString(direccion):
+                    errors.append('La dirección "' + direccion + '" del centro: ' + nombre + ' es inválida.')
+
+            # Detect cod post errors
+            cod = ''
+            if 'codi_postal' in row_data:
+                cod = row_data.get('codi_postal')
+                if not validations.isValidPostalCode(cod):
+                    errors.append('El código postal "' + cod + '" del centro: ' + cod + ' es inválido.')
+
 
             data_model = DataModel(
-                nombre= row_data.get('denominaci_completa'),
+                nombre= nombre,
                 tipo= tipo,
-                codigo_postal=row_data.get('codi_postal'),
-                direccion=row_data.get('adre_a'),
-                longitud=row_data.get('coordenades_geo_x'),
-                latitud=row_data.get('coordenades_geo_y'),
+                codigo_postal=cod,
+                direccion= direccion,
+                longitud=lon,
+                latitud=lat,
                 telefono= '',
                 descripcion= '',
                 localidad=localidad,
@@ -43,5 +77,5 @@ class XMLExtractor:
             # Agregar el objeto DataModel a la lista de datos
             data.append(data_model)
 
-        return data
+        return data, errors
 
