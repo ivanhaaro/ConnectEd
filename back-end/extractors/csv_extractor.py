84725afc08875d68539ff83bea4f3d8c825406b0
NickC25C <niccm25@gmail.com>
2023-12-04 09:20:42 +0100

    arreglitos en el csv extractor

--- back-end/extractors/csv_extractor.py
+++ back-end/extractors/csv_extractor.py
@@ -1,10 +1,13 @@
 import csv
 from scraper.scraper import CoordinateScraper
 from models.data_model import DataModel
+from extractors.validations import Validations
 
 class CSVExtractor:
     def extract_data(self, file_path):
         data = []
+        errors = []
+        validations = Validations()
         scraper = CoordinateScraper()
         with open(file_path, 'r', encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file, delimiter=';')  # Especifica el delimitador utilizado en tu CSV
@@ -13,14 +16,38 @@ class CSVExtractor:
                 try:
                     localidad = {'codigo': row['CODIGO_POSTAL'], 'nombre': row['LOCALIDAD']}
                     provincia = {'codigo': row['CODIGO_POSTAL'][:2], 'nombre': row['PROVINCIA']}
+
+                    tipoRegimen=row['REGIMEN'],
+                    if tipoRegimen == 'PÚB':
+                        tipoRegimen = 'Público'
+                    elif tipoRegimen == 'PRIV':
+                        tipoRegimen = 'Privado'
+                    elif tipoRegimen == 'PRIV.CONC.':
+                        tipoRegimen = 'Concertado'
+                    elif tipoRegimen == 'OTROS':
+                        tipoRegimen = 'Otros'
+                    else:
+                        errors.append('El tipo "' + tipoRegimen + '" es inválido')
+                        continue
+
+                    # Detect cod post errors
+                    cod = row['CODIGO_POSTAL']
+                    if not validations.isValidPostalCode(cod):
+                        errors.append('El código postal "' + cod + '" es inválido.')
+
+                    #Phone number error detection
+                    tel = row['TELEFONO']
+                    if not validations.isValidPhoneNum(tel):  
+                        errors.append('El número de teléfono "' + tel + '" es inválido.')
+
                     data_model = DataModel(
                         nombre=row['DENOMINACION'],
-                        tipo=row['TIPO_VIA'],
-                        direccion=row['DIRECCION'],
-                        codigo_postal=row['CODIGO_POSTAL'],
+                        tipo=tipoRegimen,
+                        direccion=f"{row['TIPO_VIA']} {row['DIRECCION']} {row['NUMERO']}",
+                        codigo_postal=cod,
                         longitud=0,
                         latitud=0,
-                        telefono=row['TELEFONO'],
+                        telefono=tel,
                         descripcion=row['DENOMINACION_ESPECIFICA'],
                         localidad=localidad,
                         provincia=provincia
