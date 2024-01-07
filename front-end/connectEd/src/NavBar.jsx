f23a690999610cba320e84974a2c411b91a13a44
d34dHusky <d34dHusky@users.noreply.github.com>
2024-01-07 11:33:14 +0100

    cambios frontend

---
+++ front-end/connectEd/src/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static" style={{ width: '100%' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          ConnectEd
+        </Typography>
+        <Button color="inherit">Inicio</Button>
+        <Button color="inherit">Carga de Datos</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
---
+++ front-end/connectEd/src/SearchForm.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import './SearchForm.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+
+const SearchForm = ({ onSubmit }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    onSubmit(data); 
+  };
+  const [tipoCentro, setTipoCentro] = React.useState('');
+
+  const handleChange = (event) => {
+    setTipoCentro(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              margin="normal"
+              required
+              id="locality"
+              label="Localidad"
+              name="locality"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="postalcode"
+              label="Código Postal"
+              name="CodigoPostal"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="province"
+              label="Provincia"
+              name="Provincia"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="tipo-centro-label">Tipo de Centro</InputLabel>
+              <Select
+                labelId="tipo-centro-label"
+                id="tipo-centro"
+                value={tipoCentro}
+                label="Tipo de Centro"
+                onChange={handleChange}
+              >
+                <MenuItem value={'privado'}>Privado</MenuItem>
+                <MenuItem value={'concertado'}>Concertado</MenuItem>
+                <MenuItem value={'publico'}>Público</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Buscar
+        </Button>
+      </Box>
+    </form>
+  );
+};
+
+export default SearchForm;
