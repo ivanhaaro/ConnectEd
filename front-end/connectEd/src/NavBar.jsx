03e0ee1914845e681f03a113e1097bb8f88c9e90
lanza10 <jlanza100@gmail.com>
2024-01-07 12:50:32 +0100

    linnkkkkkkkk

--- front-end/connectEd/src/NavBar.jsx
+++ front-end/connectEd/src/NavBar.jsx
@@ -3,6 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
