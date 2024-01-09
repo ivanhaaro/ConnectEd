9195f14ea1f555fff3b08c1eb94f7e9aff06cb65
Guillem <gfornetllorca@gmail.com>
2024-01-09 14:30:51 +0100

    mapdisplay

--- front-end/connectEd/src/MapDisplay.jsx
+++ front-end/connectEd/src/MapDisplay.jsx
@@ -9,18 +9,17 @@ const MapDisplay = ({ centrosEducativos }) => {
   const position = [38.9984922, -0.1654128]; // Default position, change to your needs
 
   return (
-    <Box sx={{ height: 730, width: '100%', marginTop: 2 }}>
+    <Box sx={{ height: 500, width: '100%', marginTop: 2 }}>
       <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         />
-        // Tus marcadores y popups
       </MapContainer>
       {centrosEducativos.map(centro => (
         <Marker key={centro.id} position={[centro.latitud, centro.longitud]}>
           <Popup>
-            {centro.nombre} - {centro.informacionAdicional}
+            {centro.nombre} 
           </Popup>
         </Marker>
       ))}
